7c19dbace06c0604d7453a28e02fd857c2101f9b
Robin Boers <robindev2019@outlook.com>
2023-09-20 11:12:16 +0200

    Write new parser using framework stolen from not-msb/connor :)
    He kindly rewrote it in Python for me, after which I completely ruined it LOL

--- src/parser.py
+++ src/parser.py
@@ -1,205 +1,268 @@
-from decimal import *
 from syntax.tokens import *
-from config import *
-from utils import *
 
-class Tokenizer:
-    def __init__(self, grape, source: str): 
-        self.tokens = []
-        self.line = 1
-        self.col = 1
-
-        # These are offsets for the giant ass string called 
-        # our source code. The tokenizer uses them to find the beginning 
-        # and end of tokens.
-        self.start = 0
-        self.current = 0
-
-        self.errorHandler = grape.errorHandler
-        self.source = source
-
-    def tokenize(self) -> list[Token]:
-        while not self.isAtEndOfFile():
-            self.start = self.current
-            self.scanToken()
-
-        self.tokens.append(Token(TokenType.EOF, "", None, self.line, self.col))
-        return self.tokens
-
-    def isAtEndOfFile(self) -> bool:
-        lastCharIndex = len(self.source)
-        return self.current >= lastCharIndex
-
-    def scanToken(self):
-        c = self.advance()
-
-        match c:
-            case "!":
-                if self.match("="): self.addToken(TokenType.BANG_EQUAL)
-                else: self.syntaxError(c)
-
-            case "=": self.addToken(TokenType.EQUAL_EQUAL if self.match("=") else TokenType.EQUAL)
-
-            case "<":
-                self.addToken(TokenType.LESS_EQUAL if self.match("=") else TokenType.LESS)
-            
-            case ">":
-                token = TokenType.GREATER_EQUAL if self.match("=") else TokenType.GREATER
-                self.addToken(token)
-
-            case "|":
-                token = TokenType.PIPE_ARROW if self.match(">") else TokenType.PIPE
-                self.addToken(token)
-
-            case "/":
-                if self.match("/"):
-                    self.handleComment()
-                else:
-                    self.addToken(TokenType.SLASH)
-
-            case whitespace if whitespace in [" ", "\r",  "\t"]:
-                pass # Ignore whitespace
-
-            case "\n":
-                self.addToken(TokenType.NEWLINE)
-                self.line += 1
-                self.col = 1
-
-            case "\"":
-                self.handleString()
-
-            case digit if c.isdigit():
-                self.handleNumber()
-
-            case "." if self.peek().isdigit():
-                self.handleDecimals()
-                self.addNumber()
-
-            case atom if self.isAtomChar(c):
-                self.handleAtom()
-
-            case identifier if self.isIdentifierChar(c):
-                self.handleIdentifier()
-
-            case other:
-                if self.isValidTokenType(c):
-                    self.addToken(TokenType(c))
-                else:
-                    self.syntaxError(c)
-
-    def addToken(self, token_type: TokenType, literal: any = None) -> None:
-        string = self.currentString()
-        self.tokens.append(Token(token_type, string, literal, self.line, self.start))
-
-    def syntaxError(self, unexpectedString: str = "", message: str = "", location: str = "") -> None:
-        if unexpectedString != "":
-            message = "Unexpected \"" + unexpectedString + "\". " + message
-
-        self.errorHandler.error("Syntax error", self.line, self.col, "", message)
-
-    def handleString(self):
-        while self.peek() != "\"" and not self.isAtEndOfFile():
-            if self.peek() == "/n": self.line += 1
-            self.advance()
-
-        if self.isAtEndOfFile():
-            self.syntaxError("", "Unterminated string", truncateString(self.currentString()))
-            return
-
-        # Advance once more for the closing "
-        self.advance()
-
-        literal = self.source[self.start + 1:self.current - 1]
-        self.addToken(TokenType.STRING, literal)
-
-    def handleComment(self):
-        self.skipToNextLine()
-
-    def handleNumber(self):
-        while self.peek().isdigit(): self.advance()
-        if self.match(".") and self.peek().isdigit(): self.handleDecimals()
-        self.addNumber()
-
-    def handleDecimals(self): 
-        global MAX_DECIMALS
-        currentDecimal = 0
-
-        while self.peek().isdigit(): 
-            self.advance()
-            currentDecimal += 1
-
-        if currentDecimal > MAX_DECIMALS:
-            self.errorHandler.warn(self.line, self.col, "The max amount of decimals is " + str(MAX_DECIMALS) + ". Your value is being rounded up.")   
-
-    def addNumber(self):
-        global MAX_DECIMALS
-        self.addToken(TokenType.NUMBER, round(Decimal(self.currentString()), MAX_DECIMALS))     
-
-    def handleAtom(self):
-        while self.isAlphaNumeric(self.peek()): self.advance()
-
-        literal = self.currentString()
-        self.addToken(TokenType.ATOM, literal)
-
-    def handleIdentifier(self):
-        while self.isAlphaNumeric(self.peek()): self.advance()
-
-        currentString = self.currentString()
-        if self.isValidTokenType(currentString):
-            self.addToken(TokenType(currentString))
-        else:
-            self.addToken(TokenType.IDENTIFIER)
-
-    def skipToNextLine(self):
-        while self.peek() != "\n" and not self.isAtEndOfFile():
-            self.advance()
-
-    def isAtomChar(self, c: str) -> bool:
-        return c in charRange("A", "Z") or c == "@"
-
-    def isIdentifierChar(self, c: str) -> bool:
-        return self.isAlpha(c)
-
-    def isAlpha(self, c: str) -> bool:
-        return c in charRange("a", "z") or c in charRange("A", "Z") or c == "_"
-
-    def isAlphaNumeric(self, c: str) -> bool:
-        return self.isAlpha(c) or c.isdigit()
-
-    def advance(self) -> str:
-        self.current += 1
-        self.col += 1
-        return self.currentString()
-
-    def match(self, expected: str) -> bool:
-        if self.isAtEndOfFile(): return False
-        if self.nextChar() != expected: return False
-
-        self.current += 1
-        self.col += 1
-        return True
-
-    def peek(self) -> str:
-        if self.isAtEndOfFile(): return '\0';
-        return self.nextChar()    
-    
-    def doublePeek(self) -> str:
-        afterNextChar = self.current + 1
-
-        if afterNextChar >= len(self.source): return "\0"
-        return self.charAt(afterNextChar)
-
-    def currentString(self) -> str:
-        return self.source[self.start:self.current]
-    
-    def nextChar(self) -> str:
-        return self.charAt(self.current)
-    
-    def charAt(self, index: int) -> str:
-        return self.source[index:index + 1]
-    
-    def isValidTokenType(self, string: str) -> bool:
-        return string in [token_type.value for token_type in TokenType]
-
-class ParseError(Exception):
+# ParseErroror is already used by Python itself
+class ParseError(BaseException):
     pass
+
+class Parser:
+    def __init__(self, source: str):
+        self.source = source
+        self.line = 0
+        self.col = 0
+    
+    def ignore(self, parser):
+        def parse(input):
+            output = parser(input)
+            return (output[0], None) 
+        return parse
+
+    # Match a string or match every character for
+    # which the given condition (a function) returns true.
+    def tag(self, parser):
+
+        # Match a string
+        if isinstance(parser, str):
+            def parse(input):
+                # Don't match inputs that are shorter
+                # than the parser.
+                if len(input) < len(parser):
+                    raise ParseError
+
+                if input[0:len(parser)] == parser:
+                    self.col += len(parser)
+                    return (input[len(parser):], parser)
+
+                raise ParseError
+        
+        # Match a condition (a fucntion)
+        elif callable(parser):
+            def parse(input):
+                col = self.col
+                for i in range(len(input)):
+                    col += 1
+
+                    # Loop until a character doesn't match
+                    # the given condition (a function)
+                    if not parser(input[i]):
+                        if i == 0:
+                            raise ParseError
+
+                        self.col = col
+                        return (input[i:], input[0:i])
+
+                # Don't match empty strings
+                if len(input) == 0:
+                    raise ParseError
+
+                # If the full string matches
+                # (because it didn't return in the for loop)
+                return ("", input)
+        return parse
+
+    # Run this 1 or more times.
+    def many1(self, parser):
+        def parse(input):
+            output = []
+            ok = false
+
+            while True:
+                try:
+                    p = parser(input)
+                    input = p[0]
+                    output.append(p[1])
+                    ok = true
+
+                except ParseError:
+                    break
+
+            if ok: 
+                return (input, output)
+            else: 
+                raise ParseError
+        return parse
+
+    # Run this 0 or more times.
+    def many0(self, parser):
+        def parse(input):
+            output = []
+
+            while True:
+                try:
+                    p = parser(input)
+                    input = p[0]
+                    output.append(p[1])
+
+                except ParseError:
+                    break
+
+            return (input, output)
+        return parse
+
+    # Try every parser given
+    # and return the output of the first one that works.
+    def alt(self, parsers):
+        def parse(input):
+            for parser in parsers:
+                try:
+                    return parser(input)
+
+                except ParseError:
+                    pass
+                
+            raise ParseError
+        return parse
+
+    # Match if the given parsers match in the
+    # correct order
+    def sequence(self, parsers):
+        def parse(input):
+            output = ""
+
+            for parser in parsers:
+                out = parser(input)
+                print(out)
+                input = out[0]
+                output += out[1]
+            
+            return (input, output)
+        return parse
+                    
+class Tokenizer(Parser):
+    def __init__(self, grape, source: str):
+        self.errorHandler = grape.errorHandler
+        super().__init__(source)
+
+        self.parser = self.many0(self.alt([
+            self.operator,
+            self.punctuation,
+            self.brackets,
+            self.keyword,
+            self.number,
+            self.string,    
+            self.boolean,
+            self.identifier,
+            self.whitespace,
+            self.newline
+        ]))
+
+    def tokenize(self):
+        try:
+            output = self.parser(self.source)
+            tokens = [token for token in output[1] if token is not None]
+
+            tokens.append(Token(TokenType.EOF, "", None, self.line, self.col))
+            return tokens
+
+        except ParseError as e:
+            message = e.message
+            self.errorHandler.report("Syntax error", self.line, self.col, "", message)
+
+    # Map the output of a parser to a token.
+    # Accepts an optional argument for converting the 
+    # lexeme to a literal.
+    def token(self, parser, token_type, literal = (lambda x: None)):
+        def parse(input):
+            output = parser(input)
+            lexeme = output[1]
+
+            token = Token(token_type, lexeme, literal(lexeme), self.line, self.col)
+
+            return (output[0], token)
+
+        return parse
+
+    def operator(self, input):
+        return self.alt([
+            self.token(self.tag("+"), TokenType.PLUS),
+            self.token(self.tag("-"), TokenType.MINUS),
+            self.token(self.tag("*"), TokenType.STAR),
+            self.token(self.tag("/"), TokenType.SLASH),
+            self.token(self.tag("^"), TokenType.ROOF),
+            self.token(self.tag("=="), TokenType.EQUAL_EQUAL),
+            self.token(self.tag("="), TokenType.EQUAL),
+            self.token(self.tag("!="), TokenType.BANG_EQUAL),
+            self.token(self.tag(">="), TokenType.GREATER_EQUAL),
+            self.token(self.tag(">"), TokenType.GREATER),
+            self.token(self.tag("<="), TokenType.LESS_EQUAL),
+            self.token(self.tag("<"), TokenType.LESS_EQUAL),
+            self.token(self.tag("in"), TokenType.IN),
+            self.token(self.tag("and"), TokenType.AND),
+            self.token(self.tag("not"), TokenType.NOT),
+            self.token(self.tag("or"), TokenType.OR),
+            self.token(self.tag("nor"), TokenType.NOR),
+        ])(input)
+
+    def punctuation(self, input):
+        return self.alt([
+            self.token(self.tag("\\"), TokenType.BACKSLASH),
+            self.token(self.tag("|>"), TokenType.PIPE_ARROW),
+            self.token(self.tag("|"), TokenType.PIPE),
+            self.token(self.tag("."), TokenType.DOT),
+            self.token(self.tag(":"), TokenType.DOT),
+            self.token(self.tag(","), TokenType.COMMA)
+        ])(input)
+
+    def brackets(self, input):
+        return self.alt([
+            self.token(self.tag("("), TokenType.LEFT_PAREN),
+            self.token(self.tag(")"), TokenType.RIGHT_PAREN),
+            self.token(self.tag("{"), TokenType.LEFT_BRACE),
+            self.token(self.tag("}"), TokenType.RIGHT_BRACE),
+            self.token(self.tag("["), TokenType.LEFT_BRACKET),
+            self.token(self.tag("]"), TokenType.RIGHT_BRACKET)
+        ])(input)
+
+    def keyword(self, input):
+        return self.alt([
+            self.token(self.tag("fn"), TokenType.FN),
+            self.token(self.tag("if"), TokenType.IF),
+            self.token(self.tag("else"), TokenType.ELSE),
+            self.token(self.tag("do"), TokenType.DO),
+            self.token(self.tag("end"), TokenType.END),
+
+            # Namespaces
+            self.token(self.tag("namespace"), TokenType.NAMESPACE),
+            self.token(self.tag("use"), TokenType.USE),
+            self.token(self.tag("import"), TokenType.IMPORT),
+            self.token(self.tag("pub"), TokenType.PUB),
+            self.token(self.tag("@"), TokenType.AT),
+            self.token(self.tag("$"), TokenType.EXEC)
+        ])(input)    
+
+    def number(self, input):
+        return self.token(self.tag(isDigit), TokenType.NUMBER, int)(input)
+
+    def string(self, input):
+        return self.token(self.sequence([
+            self.tag("\""), 
+            self.tag(lambda c: c != "\""), 
+            self.tag("\"")
+        ]), TokenType.STRING, str)(input)
+
+    def boolean(self, input):
+        return self.alt([
+            self.token(self.tag("true"), TokenType.TRUE, bool),
+            self.token(self.tag("false"), TokenType.FALSE, bool)
+        ])(input)
+
+    def identifier(self, input):
+        return self.token(self.tag(lambda c: isAlphaNumeric(c) or c in ["_"]), TokenType.IDENTIFIER)(input)
+
+    def whitespace(self, input):
+        return self.alt([
+            self.ignore(self.tag(" ")),
+            self.ignore(self.tag("\r")),
+            self.ignore(self.tag("\t"))
+        ])(input)
+
+    def newline(self, input):
+        return self.token(self.tag("\n"), TokenType.NEWLINE)(input)
+
+def isAlpha(char):
+    return char.isalpha()
+    
+def isDigit(char):
+    return char.isdigit()
+
+def isAlphaNumeric(char):
+    return isAlpha(char) or isDigit(char)
